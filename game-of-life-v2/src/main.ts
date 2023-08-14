2dc5bfd40c79a7e755a13ea815756366ac3aa09d
KenTandrian <kenricktan11@gmail.com>
2023-08-14 10:19:42 +0700

    fix(app): remove usage of innerHTML

--- game-of-life-v2/src/main.ts
+++ game-of-life-v2/src/main.ts
@@ -1,14 +1,4 @@
 import "./style.css";
 import { setupGame } from "./game";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <h1>Conway's Game of Life</h1>
-  <canvas width="1024" height="512"></canvas>
-  <div>
-    <button onclick="window.location.reload();">
-      Restart
-    </button>
-  </div>
-`;
-
 setupGame(document.querySelector<HTMLCanvasElement>("canvas")!);
