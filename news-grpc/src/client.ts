0f97ccc5b4104ab2856062212a4bafeb86991b0a
KenTandrian <kenricktan11@gmail.com>
2023-08-20 15:12:11 +0700

    fix(grpc): remove manual proto loader

--- news-grpc/src/client.ts
+++ news-grpc/src/client.ts
@@ -1,17 +1,5 @@
 import * as grpc from "@grpc/grpc-js";
-import * as protoLoader from "@grpc/proto-loader";
 import { NewsServiceClient } from "../dist/news";
-
-const PROTO_PATH = "./proto/news.proto";
-const options = {
-  keepCase: true,
-  longs: String,
-  enums: String,
-  defaults: true,
-  oneofs: true,
-};
-var packageDefinition = protoLoader.loadSync(PROTO_PATH, options);
-const NewsService = grpc.loadPackageDefinition(packageDefinition).NewsService;
 
 const client = new NewsServiceClient(
   "localhost:50051",
--- news-grpc/src/server.ts
+++ news-grpc/src/server.ts
@@ -1,22 +1,9 @@
 import * as grpc from "@grpc/grpc-js";
-import * as protoLoader from "@grpc/proto-loader";
 import { addNews, deleteNews, editNews, getAllNews, getNews } from "./services";
-
-const PROTO_PATH = "./proto/news.proto";
-const options: protoLoader.Options = {
-  keepCase: true,
-  longs: String,
-  enums: String,
-  defaults: true,
-  oneofs: true,
-};
-var packageDefinition = protoLoader.loadSync(PROTO_PATH, options);
-const newsProto = grpc.loadPackageDefinition(packageDefinition);
-const newsService = newsProto.NewsService as grpc.ServiceClientConstructor;
+import { NewsServiceService } from "../dist/news";
 
 const server = new grpc.Server();
-
-server.addService(newsService.service, {
+server.addService(NewsServiceService, {
   getAllNews,
   getNews,
   addNews,
