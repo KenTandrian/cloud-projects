c44f42c70dfa6cc722481f4f0043b2cf5c07462b
KenTandrian <kenricktan11@gmail.com>
2023-08-20 12:07:04 +0700

    feat(grpc): create client

---
+++ src/client.js
@@ -0,0 +1,23 @@
+const grpc = require("@grpc/grpc-js");
+const protoLoader = require("@grpc/proto-loader");
+
+const PROTO_PATH = "./proto/news.proto";
+const options = {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+};
+var packageDefinition = protoLoader.loadSync(PROTO_PATH, options);
+const NewsService = grpc.loadPackageDefinition(packageDefinition).NewsService;
+
+const client = new NewsService(
+  "localhost:50051",
+  grpc.credentials.createInsecure()
+);
+
+client.getAllNews({}, (error, news) => {
+  if (error) throw error;
+  console.log(news);
+});
